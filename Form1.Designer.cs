ab82280a3271d79be3925145166f4d9dc0ccf6cf
Gerson <gersonzorin@mesoamericana.edu.gt>
2022-04-26 21:35:24 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace GeneradorVerificadorCodigo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtGenerador = new System.Windows.Forms.TextBox();
+            this.txtVerificador = new System.Windows.Forms.TextBox();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.lbControl = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtGenerador
+            // 
+            this.txtGenerador.Location = new System.Drawing.Point(76, 94);
+            this.txtGenerador.Margin = new System.Windows.Forms.Padding(4);
+            this.txtGenerador.Name = "txtGenerador";
+            this.txtGenerador.Size = new System.Drawing.Size(148, 22);
+            this.txtGenerador.TabIndex = 0;
+            // 
+            // txtVerificador
+            // 
+            this.txtVerificador.Location = new System.Drawing.Point(76, 214);
+            this.txtVerificador.Margin = new System.Windows.Forms.Padding(4);
+            this.txtVerificador.Name = "txtVerificador";
+            this.txtVerificador.Size = new System.Drawing.Size(148, 22);
+            this.txtVerificador.TabIndex = 1;
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(382, 94);
+            this.btnGenerar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(124, 50);
+            this.btnGenerar.TabIndex = 2;
+            this.btnGenerar.Text = "Generar Codigo";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(382, 194);
+            this.btnVerificar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(124, 44);
+            this.btnVerificar.TabIndex = 3;
+            this.btnVerificar.Text = "Verificar Codigo";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lbControl
+            // 
+            this.lbControl.AutoSize = true;
+            this.lbControl.Location = new System.Drawing.Point(76, 42);
+            this.lbControl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbControl.Name = "lbControl";
+            this.lbControl.Size = new System.Drawing.Size(289, 16);
+            this.lbControl.TabIndex = 4;
+            this.lbControl.Text = "Vector de control: { 9, 8, 7, 6, 5, 4, 3, 2, 1 }";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Generar Codigo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(76, 194);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Verificar Codigo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(687, 304);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbControl);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.txtVerificador);
+            this.Controls.Add(this.txtGenerador);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtGenerador;
+        private System.Windows.Forms.TextBox txtVerificador;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.Label lbControl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeneradorVerificadorCodigo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent(); 
+        }
+
+        List<int> vectorControl = new List<int>() { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+        List<int> vectordatos = new List<int>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string datos = txtGenerador.Text;
+            string generarTemp = generador(datos);
+            MessageBox.Show("codigo \n" + generarTemp);
+            vectordatos.Clear();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string datos = txtVerificador.Text;
+            string veriTemp = verificador(datos);
+            MessageBox.Show(veriTemp);
+            vectordatos.Clear();
+        }
+
+        private string verificador(string datos)
+        {
+            int digitoVerificador = 0;
+            double temp = 0;
+            double multiplo = 0;
+            double aux = 0;
+            double aux2 = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                char num = datos[i];
+                int num1 = (int)Char.GetNumericValue(num);
+                vectordatos.Add(num1);
+            }
+            temp = (vectordatos[0] * vectorControl[0] + vectordatos[1] * vectorControl[1] + vectordatos[2] * vectorControl[2] + vectordatos[3] * vectorControl[3] + vectordatos[4]
+                * vectorControl[4] + vectordatos[5] * vectorControl[5] + vectordatos[6] * vectorControl[6] + vectordatos[7] * vectorControl[7] + vectordatos[8] * vectorControl[8]);
+            aux = temp / 10;
+            multiplo = (double)Math.Ceiling(aux);
+            aux2 = multiplo - aux;
+            digitoVerificador = (int)(aux2 * 10);
+            char verificar = datos[9];
+            string vertemp = Convert.ToString(digitoVerificador);
+            char verdig = vertemp[0];
+            if (verificar == verdig)
+            {
+                string msj = "codigo correcto ";
+                this.BackColor = Color.Green;
+                lbControl.ForeColor = Color.White;
+                label1.ForeColor = Color.White;
+                label2.ForeColor = Color.White;
+                return msj;
+            }
+            else
+            {
+                string msj = "codigo incorrecto ";
+                this.BackColor = Color.Red;
+                lbControl.ForeColor = Color.White;
+                label1.ForeColor = Color.White;
+                label2.ForeColor = Color.White;
+                return msj;
+            }
+        }
+
+        private string generador(string datos)
+        {
+            int digitoVerificador = 0;
+            double temp = 0;
+            double multiplo = 0;
+            double aux = 0;
+            double aux2 = 0;
+            for (int i = 0; i < datos.Length; i++)
+            {
+                char num = datos[i];
+                int num1 = (int)Char.GetNumericValue(num);
+                vectordatos.Add(num1);
+            }
+            temp = (vectordatos[0] * vectorControl[0] + vectordatos[1] * vectorControl[1] + vectordatos[2] * vectorControl[2] + vectordatos[3] * vectorControl[3] + vectordatos[4]
+                * vectorControl[4] + vectordatos[5] * vectorControl[5] + vectordatos[6] * vectorControl[6] + vectordatos[7] * vectorControl[7] + vectordatos[8] * vectorControl[8]);
+            aux = temp / 10;
+            multiplo = (double)Math.Ceiling(aux);
+            aux2 = multiplo - aux;
+            digitoVerificador = (int)(aux2 * 10);
+            string retornar = datos + digitoVerificador;
+            return retornar;
+        }
+    }
+}
